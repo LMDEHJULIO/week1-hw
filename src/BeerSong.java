793c82691f0ae0263be30cca799c4e915d8f490a
Julio Rodriguez <juliodevelops@gmail.com>
2023-09-12 10:20:51 -0400

    Simplify beer song

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -1,30 +1,16 @@
 public class BeerSong {
     public static void main(String[] args) {
-        int beerNum = 99;
-
-        String word = "bottles";
-
-        while(beerNum > 0){
-            if(beerNum == 1){
-                word = "bottle";
-            }
-
-            System.out.println(beerNum + " " + word + " of beer on the wall");
-
-            System.out.println(beerNum + " " + word + " of beer.");
-
-            System.out.println("Take one down.");
-
-            System.out.println("Pass it around.");
-
-            System.out.println(beerNum + " " + word + " of beer on the wall");
-
-            beerNum -= 1;
-
-            if(beerNum == 0){
-                System.out.println("No more bottles of beer on the wall");
-            }
-
+        for(int beerNum = 99; beerNum >= 0; beerNum--){
+            if(beerNum == 0) { zeroPrint(); } else {beerPrint(beerNum);}
         }
     }
+    private static void beerPrint(int beerNum){
+        String word = beerNum > 1 ? "bottles" : "bottle";
+        System.out.printf("%d %s of beer on the wall. \n %d %s of beer. \n Take one down. \n Pass it around. \n", beerNum, word, beerNum, word);
+    }
+
+    private static void zeroPrint(){
+        System.out.println("No more bottles of beer on the wall");
+    }
+
 }
