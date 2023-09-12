8ebc27ae5d5b02c3c153730e6ec396542294417d
Julio Rodriguez <juliodevelops@gmail.com>
2023-09-12 13:46:46 -0400

    Update BeerSong w/ MessageFormat

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -1,16 +1,13 @@
+import java.text.MessageFormat;
+
 public class BeerSong {
     public static void main(String[] args) {
-        for(int beerNum = 99; beerNum >= 0; beerNum--){
-            if(beerNum == 0) { zeroPrint(); } else {beerPrint(beerNum);}
+        for(int beerNum = 99; beerNum > 0; beerNum--){
+            String word = beerNum > 1 ? "bottles" : "bottle";
+
+            String endString = beerNum > 1 ? beerNum - 1 + " bottles of beer on the wall." : "No more bottles of beer on the wall.";
+
+            System.out.println(MessageFormat.format("{0} {1} of beer on the wall.\n {0} {1}  of beer \n Take one down. \n Pass it around \n {2}", beerNum, word, endString));
         }
     }
-    private static void beerPrint(int beerNum){
-        String word = beerNum > 1 ? "bottles" : "bottle";
-        System.out.printf("%d %s of beer on the wall. \n %d %s of beer. \n Take one down. \n Pass it around. \n", beerNum, word, beerNum, word);
-    }
-
-    private static void zeroPrint(){
-        System.out.println("No more bottles of beer on the wall");
-    }
-
 }
